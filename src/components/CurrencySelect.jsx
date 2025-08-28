ad2760a1efd1e2510e0b3cbec7d03be3dc709caf
Abhishek kumar <109682053+Abhishekkkkkkkkkkk@users.noreply.github.com>
2025-08-28 18:28:17 +0530

    Currency Select

--- src/components/CurrencySelect.jsx
+++ src/components/CurrencySelect.jsx
@@ -1,13 +1,14 @@
 import { useMemo } from "react";
 import { currencyCodes } from "../utils/currencyList.js";
 
-
 // Accessible dropdown with a clean list of common ISO 4217 codes
 export default function CurrencySelect({ id, value, onChange }) {
   const labels = useMemo(() => {
     let dn;
     try {
-      dn = new Intl.DisplayNames([navigator.language || "en"], { type: "currency" });
+      dn = new Intl.DisplayNames([navigator.language || "en"], {
+        type: "currency",
+      });
     } catch {
       dn = null;
     }
